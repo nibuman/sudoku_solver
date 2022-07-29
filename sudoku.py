5d9b6c4fed19b3a89c1873e3936663d78bab94a7
Nick Bushby <nick.bushby5@gmail.com>
2022-07-29 16:17:00 +0100

    Working

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,4 @@
-__version__ = "2.1"
+__version__ = "2.11"
 
 import time
 import copy
@@ -12,20 +12,18 @@ def display_board(board_list: list) -> None:
     for a in range(0, 81, 27):
         for b in range(0, 27, 9):
             n = a + b
-
             print(board[n:n+3], " ", board[n+3:n+6], " ", board[n+6:n+9])
         print(" ")
 
 
-def update_available(i: int, n: str, cell_defn: tuple) -> None:
+def update_available(board_pos: int, number: str, cell_defn: tuple) -> None:
     """Update rows, columns and squares by removing unavailable numbers"""
     for j in range(3):
-        cell_defn[i][j].discard(n)
+        cell_defn[board_pos][j].discard(number)
 
 
 def clean_string(board_string):
     """Reformats a text string as a valid board definition
-
     - will remove any characters that are not 0-9
     """
     allowed_vals = {str(n) for n in range(10)}
@@ -65,10 +63,9 @@ def solve_sudoku(board: list, rec_depth: int, use_alg2: bool = True) -> list:
                   706 716 726   737 747 757   768 778 788
                   806 816 826   837 847 857   868 878 888"""
     # format string as (({row}, {col}, {sq}, [r1], [c1], [s1]),...)
-    defn = [(r[int(x)], c[int(y)], s[int(z)],
-            r1[int(x)], c1[int(y)], s1[int(z)])
-            for x, y, z in defn_str.split()]
-    defn = tuple(defn)
+    defn = tuple([(r[int(x)], c[int(y)], s[int(z)],
+                  r1[int(x)], c1[int(y)], s1[int(z)])
+                  for x, y, z in defn_str.split()])
 
     for i, n in enumerate(board):
         update_available(i, n, defn)
@@ -108,7 +105,7 @@ def solve_sudoku(board: list, rec_depth: int, use_alg2: bool = True) -> list:
         # run the second algorithm - each row, col, sq must have 1 of
         # all 9 numbers. Cannot run if there have been changes made as
         # r1, c1, and s1 will not be up-to-date
-        if use_alg2 is True:
+        if changed is False and use_alg2 is True:
             for row in (r1 + c1 + s1):
                 for pos, available_pos in enumerate(row):
                     if len(available_pos) == 1:
